ff970294c0821574455030bafcd01777e1c826ef
CrispinSouth <47156832+CrispinSouth@users.noreply.github.com>
2019-02-19 20:32:46 -0600

    Commit 1

---
+++ Todo.java
@@ -0,0 +1,24 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-19
+OU ID_______________: 113358570
+OU EMAIL____________: Crispin@ou.edu
+
+Tasks:
+1. Calculate two tokens method
+2. Calculate three tokens method
+3. Execute method
+4. Parse and execute method
+5. Multiple methods in CalculatorTest Class
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 10
+5. 40
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+*/}
